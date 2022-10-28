6898bb67366362850dd14ee5e40f8389f681660d
MAGESH H <hariarunengg@gmail.com>
2022-10-28 09:39:24 +0530

    refactoring

--- Main/Main.java
+++ Main/Main.java
@@ -1,28 +1,10 @@
 package Main;
 
-<<<<<<< HEAD
-=======
-import java.util.Scanner;
-import Login.*;
-import ThankYou.ThankYouScreen;
-
-import OthersScreen.OthersScreen;
->>>>>>> b9bc5cc764ffbcf12b7dcb306d1925938eb5e5f8
 
 public class Main {
 
     public static void main(String[] args) {
 
-<<<<<<< HEAD
-=======
-//         new NewRegister();
-//         new LoginPage();
-//            new ThankYouScreen();
-//        IdPass idPass = new IdPass();
-//        LoginPage login = new LoginPage(idPass.getLoginInfo());
->>>>>>> b9bc5cc764ffbcf12b7dcb306d1925938eb5e5f8
-
-        new OthersScreen();
 
     }
 }
